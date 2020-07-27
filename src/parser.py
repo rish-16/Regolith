a9bb77fba943f7530a3307e10f500588081405d0
rish-16 <mail.rishabh.anand@gmail.com>
2020-07-27 21:57:03 +0800

    Add basic Parser

--- src/parser.py
+++ src/parser.py
@@ -10,6 +10,28 @@ class Parser:
     def parse(self):
         @self.pg.production('program : PRINT OPEN_PAREN expression CLOSE_PAREN SEMI_COLON')
         def program(p):
-            return Print(p)
+            return Print(p[2])
             
-        
+        @self.pg.production('expression : expression SUM expression')
+        @self.pg.production('expression : expression SUB expression')
+        def expression(p):
+            left = p[0]
+            right = p [1]
+            
+            operator = p[2]
+            
+            if operator.gettokentype() == 'SUM':
+                return Sum(left, right)
+            elif operator.gettokentype() == 'SUB':
+                return Sub(left, right)
+                
+        @self.pg.production('expression : NUMBER')
+        def number(p):
+            return Number(p[0].value)
+            
+        @self.pg.error
+        def error_handle(token):
+            return ValueError(token)
+            
+    def get_parser(self):
+        return self.pg.build()
